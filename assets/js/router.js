e293160325dcfcdb981e5678d2145d2312c930f1
elaraks <elarashurety@gmail.com>
2022-09-13 13:09:06 +0100

    Update all

--- assets/js/router.js
+++ assets/js/router.js
@@ -5,6 +5,10 @@ import {
   router,
 } from "https://unpkg.com/million@1.11.5/dist/router.mjs"
 
+const router {
+  let link = window.open(url, "_blank");
+}
+
 export const attachSPARouting = (init, rerender) => {
   // Attach SPA functions to the global Million namespace
   window.Million = {
